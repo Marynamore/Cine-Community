8390ca731759bb8c99bc28ecae837f349512619f
Marina Oliveira <86386469+Marynamore@users.noreply.github.com>
2024-02-22 23:30:15 -0300

    Delete control directory

--- control/control_sairadm.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if($_SESSION['perfil_usu'] == "administrador"){
-unset($_SESSION['id_usuario']);
-}
-header("Location: ../index.php");
-
-?>
--- control/sair_usu_action.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['id_usuario'], $_SESSION['nickname_usu']);
-session_destroy();
-header("Location: ../index.php");
-
-?>
